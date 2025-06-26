ead3afb4312adb91b2dd7237492dd054fe8201e7
TipDevs <tipdevs@gmail.com>
2025-06-26 11:58:51 +0100

    Import images from src/assets/images and store them in array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import "./styles/home.css"
+import "./styles/home.css";
+import jollofRice from "./assets/images/jollof_rice.webp";
+import asunPasta from "./assets/images/asun_pasta.webp";
+import seaFoodRice from "./assets/images/seafood_rice.webp";
+import efoRiro from "./assets/images/efo_riro.webp";
+import catFishPepperSoup from "./assets/images/cat_fish_pepper_soup.webp";
+const foodImages = [jollofRice, asunPasta, seaFoodRice, efoRiro, catFishPepperSoup];
