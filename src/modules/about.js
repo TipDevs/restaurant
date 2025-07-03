c70364b65aea7a87c2f78474803148e494dcc1fd
TipDevs <tipdevs@gmail.com>
2025-07-03 18:48:47 +0100

    Ensure the line of codes are display correctly

--- src/modules/about.js
+++ src/modules/about.js
@@ -1,11 +1,12 @@
 import ceo_photo from "../assets/images/ceo.webp";
+import "../styles/assets/about.css"
 export function aboutUI() {
     let about_main = document.querySelector('main');
     about_main.classList.add('about_main');
     const aboutCeo = {
         heading: "About the CEO",
         ceo_photo: ceo_photo,
-        title: "Chef Sarah Anthony Moyosore — Founder & CEO, Aila’s Kitchen",
+        title: "Chef Sarah Anthony Moyosore Founder & CEO, Aila’s Kitchen",
         firstPara: "Chef Sarah Anthony Moyosore is the culinary mind and founder of Aila’s Kitchen, a proudly Nigerian food brand dedicated to delivering authentic, flavorful, and expertly prepared meals. With several years of professional kitchen experience, Chef Sarah has built a reputation for her skillful blend of traditional recipes and contemporary culinary techniques.",
         secondPara: "Her career in the food industry began with hands-on roles in some of Nigeria’s well-regarded food establishments. She served as a Cook at Donald Fast Food by De Angel’s, where she gained valuable experience in fast-paced kitchen environments and customer-focused service. She went on to work as a Chef at Naija Pasta & Grills, where she further refined her mastery in preparing specialty rice and pasta dishes, becoming known for her creativity and attention to detail.",
         thirdPara: "Beyond commercial kitchens, Chef Sarah also held a Private Cook position for a family of four, a full-time live-out role where she handled diverse daily meals, ranging from indigenous soups to intercontinental dishes — a role that honed her versatility and personalized culinary service.",
@@ -22,28 +23,44 @@ export function aboutUI() {
         ]
     }
     function writeAboutCeo() {
+        let aboutCeoBox = document.createElement('div');
+        aboutCeoBox.classList.add('about_ceo_box');
         let headingText = document.createElement('h2');
-        headingText.textContent = `${aboutCeo.heading}`
+        headingText.textContent = aboutCeo.heading;
         let ceo_photo = document.createElement('img');
         ceo_photo.src = aboutCeo.ceo_photo;
         let ceoTitle = document.createElement('h3');
         ceoTitle.textContent = aboutCeo.title;
         let firstPara = document.createElement('p');
-        firstPara.textContent = `${aboutCeo.firstPara}`;
+        firstPara.textContent = aboutCeo.firstPara;
         let secondPara = document.createElement('p');
-        secondPara.textContent = `${aboutCeo.secondPara}`;
+        secondPara.textContent = aboutCeo.secondPara;
         let thirdPara = document.createElement('p');
-        thirdPara.textContent = `${aboutCeo.thirdPara}`;
+        thirdPara.textContent = aboutCeo.thirdPara;
         let ceoPlanPara = document.createElement('p');
-        ceoPlanPara.textContent = `${aboutCeo.ceoPlan[0]}`;
+        ceoPlanPara.textContent = aboutCeo.ceoPlan[0];
         let ceoPlanList = document.createElement('ul');
         let plan1 = document.createElement('li');
-        plan1.textContent = `${aboutCeo.ceoPlan[1]}`;
+        plan1.textContent = aboutCeo.ceoPlan[1];
         let plan2 = document.createElement('li');
-        plan2.textContent = `${aboutCeo.ceoPlan[2]}`;
+        plan2.textContent = aboutCeo.ceoPlan[2];
         let plan3 = document.createElement('li');
-        plan3.textContent = `${aboutCeo.ceoPlan[3]}`;
+        plan3.textContent = aboutCeo.ceoPlan[3];
         let plan4 = document.createElement('li');
-        plan4.textContent = `${aboutCeo.ceoPlan[4]}`;
+        plan4.textContent = aboutCeo.ceoPlan[4];
+        aboutCeoBox.appendChild(headingText);
+        aboutCeoBox.appendChild(ceo_photo);
+        aboutCeoBox.appendChild(ceoTitle);
+        aboutCeoBox.appendChild(firstPara);
+        aboutCeoBox.appendChild(secondPara);
+        aboutCeoBox.appendChild(thirdPara);
+        aboutCeoBox.appendChild(ceoPlanPara);
+        ceoPlanList.appendChild(plan1);
+        ceoPlanList.appendChild(plan2);
+        ceoPlanList.appendChild(plan3);
+        ceoPlanList.appendChild(plan4);
+        aboutCeoBox.appendChild(ceoPlanList);
+        about_main.appendChild(aboutCeoBox);
     }
+    writeAboutCeo();
 }
