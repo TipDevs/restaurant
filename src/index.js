e35a250369a7f6ca17fb0be5a79884468a83d8c5
TipDevs <tipdevs@gmail.com>
2025-07-03 23:41:53 +0100

    Add new script for button clicks and navigations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,43 @@
 import "./styles/static.css";
 import { homeUI } from "./modules/home.js";
 import { menuUI } from "./modules/menu.js";
+import { aboutUI } from "./modules/about.js";
+let homeBtn = document.querySelectorAll('.home_btn');
+let menuBtn = document.querySelectorAll('.menu_btn');
+let aboutBtn = document.querySelectorAll('.about_btn');
+let body = document.querySelector('body');
+let main = document.querySelector('main');
+let hamburger = document.querySelector('.hamburger');
+let hamburgerNav = document.querySelector('.hamburger_nav');
+document.addEventListener('DOMContentLoaded', homeUI);
+hamburger.addEventListener('click', () => {
+    hamburgerNav.classList.toggle('active');
+})
+hamburgerNav.querySelectorAll('button').forEach(btn => {
+    btn.addEventListener('click', () => {
+        hamburgerNav.classList.remove('active');
+    })
+})
   
-homeUI();
-// menuUI();
+homeBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        main.classList.remove('menu_main', 'about_main');
+        main.innerHTML = '';
+        homeUI();
+    });
+})
+menuBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        main.classList.remove('home_main', 'about_main');
+        main.innerHTML = '';
+        menuUI();
+    });
+})
+aboutBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        main.classList.remove('home_main', 'menu_main');
+        main.innerHTML = '';
+        aboutUI();
+    });
+})
+
